eaa8b6f1828c85dae04de85947cad248333ff565
wisnu <wisnu_dagonza8@yahoo.co.id>
2016-07-20 09:55:42 +0700

    add invitations page

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -10,8 +10,7 @@ export default Ember.Controller.extend({
       var _that = this;
       var email = this.get('emailAddress');
     	var newInvitation = this.store.createRecord('invitation', {email: email});
-    	newInvitation.save().then((response){
-        console.log(response);
+    	newInvitation.save().then(function(response){
         _that.set('responseMessage', `Thank you! We've just saved your email address: ${_that.get('emailAddress')}`);
         _that.set('emailAddress', '');
       });
