3ca61a55c31b8394e0f49de324673fd1f82fdcca
wisnu <wisnu@gopherindonesia.com>
2016-07-21 09:38:10 +0700

    clean up the codes with component

--- app/models/library.js
+++ app/models/library.js
@@ -1,9 +1,11 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
+import Ember from 'ember';
 // import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
   name: attr('string'),
   address: attr('string'),
-  phone: attr('string')
+  phone: attr('string'),
+  isValid: Ember.computed.notEmpty('name')
 });
--- app/routes/libraries/edit.js
+++ app/routes/libraries/edit.js
@@ -3,7 +3,18 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model(params) {
-    return this.store.findRecord('library', params.id);
+    return this.store.findRecord('library', params.library_id);
+  },
+
+  setupController(controller, model) {
+    this._super(controller, model);
+
+    controller.set('title', 'Edit library');
+    controller.set('buttonLabel', 'Save changes');
+  },
+
+  renderTemplate() {
+    this.render('libraries/form');
   },
 
   actions: {
@@ -13,7 +24,6 @@ export default Ember.Route.extend({
     },
 
     willTransition(transition) {
-
       let model = this.controller.get('model');
 
       if (model.get('hasDirtyAttributes')) {
